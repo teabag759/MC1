c2e5ed3346f693ce2bcd820a2aebacb84d6a4cc2
piri <pyoram25@gmail.com>
2024-03-23 11:03:03 +0900

    [Feat]CardTestView생성

--- MC1/View/CardTestView.swift
+++ MC1/View/CardTestView.swift
@@ -2,25 +2,7 @@
 //  CardTestView.swift
 //  MC1
 //
-//  Created by ram on 3/21/24.
+//  Created by ram on 3/23/24.
 //
 
-//import Foundation
-//import SwiftUI
-//
-//
-//
-//struct CardTestView: View {
-//    var body: some View {
-//        NavigationView {
-//            CardView()
-//                .navigationBarTitle("카드 그룹")
-//        }
-//    }
-//}
-//
-//struct MainView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        CardTestView()
-//    }
-//}
+import Foundation
