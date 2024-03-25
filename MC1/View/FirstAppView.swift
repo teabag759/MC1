8d3a3bfd1eecb549911cf5ec268d3a53960ff3b8
piri <pyoram25@gmail.com>
2024-03-25 11:29:36 +0900

    [Refactor]주석삭제

--- MC1/View/FirstAppView.swift
+++ MC1/View/FirstAppView.swift
@@ -15,43 +15,9 @@ struct FirstAppView: View {
             NavigationLink(destination: SurveyView()) {
                 Text("두 번째 뷰로 이동")
             }
-            //            ZStack {
-            //                Color.clear
-            //                    .contentShape(Rectangle()) // 화면 전체에 대한 탭을 감지하기 위해
-            //                    .onTapGesture {
-            //                        self.isActiveSurveyView = true
-            //                    }
-            //
-            //                VStack {
-            //                    Text("뭐해볼까?")
-            //                }
-            //            }
-            //            .fullScreenCover(isPresented: $isActiveSurveyView) {
-            //                SurveyView()
         }
-        //        }
     }
 }
-//struct FirstAppView: View {
-//    @State private var isActiveSurveyView = false
-//
-//    var body: some View {
-//        ZStack {
-//            Color.clear
-//                .contentShape(Rectangle()) // 화면 전체에 대한 탭을 감지하기 위해
-//                .onTapGesture {
-//                    self.isActiveSurveyView = true
-//                }
-//
-//            VStack {
-//                Text("뭐해볼까?")
-//            }
-//        }
-//        .fullScreenCover(isPresented: $isActiveSurveyView) {
-//            SurveyView()
-//        }
-//    }
-//}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
